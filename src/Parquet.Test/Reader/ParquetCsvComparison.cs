ae4caca82a885f88bc41793fb4af31e4b324c5c3
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-06-26 10:04:47 +0100

    reading parquet, csv, comparing number of columns. Added spark generated csv.

---
+++ src/Parquet.Test/Reader/ParquetCsvComparison.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using Path = System.IO.Path;
+using F = System.IO.File;
+using NetBox.FileFormats;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Parquet.Test.Reader
+{
+   public class ParquetCsvComparison
+   {
+      protected void CompareFiles(string baseName)
+      {
+         ParquetDataSet parquet = ReadParquet(baseName + ".parquet");
+         var csv = ReadCsv(baseName + ".csv");
+         Compare(parquet, csv);
+      }
+
+      private void Compare(ParquetDataSet parquet, Dictionary<string, List<string>> csv)
+      {
+         Assert.Equal(parquet.Columns.Count, csv.Count);
+      }
+
+      private ParquetDataSet ReadParquet(string name)
+      {
+         ParquetDataSet parquet;
+         using (Stream fs = F.OpenRead(GetDataFilePath(name)))
+         {
+            using (ParquetReader reader = new ParquetReader(fs))
+            {
+               parquet = reader.Read();
+            }
+         }
+         return parquet;
+      }
+
+      private Dictionary<string, List<string>> ReadCsv(string name)
+      {
+         var result = new Dictionary<string, List<string>>();
+         using (Stream fs = F.OpenRead(GetDataFilePath(name)))
+         {
+            var reader = new CsvReader(fs, Encoding.UTF8);
+
+            //header
+            string[] columnNames = reader.ReadNextRow();
+            foreach (string columnName in columnNames) result[columnName] = new List<string>();
+
+            //values
+            string[] values;
+            while((values = reader.ReadNextRow()) != null)
+            {
+               for(int i = 0; i < values.Length; i++)
+               {
+                  result[columnNames[i]].Add(values[i]);
+               }
+            }
+         }
+         return result;
+      }
+
+      private string GetDataFilePath(string name)
+      {
+         string thisPath = Assembly.Load(new AssemblyName("Parquet.Test")).Location;
+         return Path.Combine(Path.GetDirectoryName(thisPath), "data", name);
+      }
+   }
+}
---
+++ src/Parquet.Test/Reader/TestDataTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Parquet.Test.Reader
+{
+   public class TestDataTest : ParquetCsvComparison
+   {
+      [Fact]
+      public void Alltypes_plain()
+      {
+         CompareFiles("alltypes_plain");
+      }
+   }
+}
