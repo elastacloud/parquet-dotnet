1dfc7a1bc5da416238eb316f0a8399d8d166e64d
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-06-26 10:16:43 +0100

    validate column types

--- src/Parquet.Test/Reader/ParquetCsvComparison.cs
+++ src/Parquet.Test/Reader/ParquetCsvComparison.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using System.Text;
 using Path = System.IO.Path;
 using F = System.IO.File;
+using Type = System.Type;
 using NetBox.FileFormats;
 using System.Collections.Generic;
 using Xunit;
@@ -11,16 +12,37 @@ namespace Parquet.Test.Reader
 {
    public class ParquetCsvComparison
    {
-      protected void CompareFiles(string baseName)
+      protected void CompareFiles(string baseName, params Type[] columnTypes)
       {
          ParquetDataSet parquet = ReadParquet(baseName + ".parquet");
          var csv = ReadCsv(baseName + ".csv");
-         Compare(parquet, csv);
+         Compare(parquet, csv, columnTypes);
       }
 
-      private void Compare(ParquetDataSet parquet, Dictionary<string, List<string>> csv)
+      private void Compare(ParquetDataSet parquet, Dictionary<string, List<string>> csv, Type[] columnTypes)
       {
+         //compar number of columns is the same
          Assert.Equal(parquet.Columns.Count, csv.Count);
+
+         //compare column names
+         foreach(ParquetColumn pq in parquet.Columns)
+         {
+            Assert.True(csv.ContainsKey(pq.Name));
+         }
+
+         //compare column values one by one
+         Assert.True(columnTypes.Length == csv.Count, $"incorrect type count, expected {csv.Count} but found {columnTypes.Length}");
+
+         //compare individual columns
+         int i = 0;
+         foreach(ParquetColumn pc in parquet.Columns)
+         {
+            List<string> cc = csv[pc.Name];
+            Type expectedColumnType = columnTypes[i++];
+
+            //validate column type
+            Assert.True(expectedColumnType == pc.SystemType, $"expected {expectedColumnType} for column {pc.Name} but found {pc.SystemType}");
+         }
       }
 
       private ParquetDataSet ReadParquet(string name)
--- src/Parquet.Test/Reader/TestDataTest.cs
+++ src/Parquet.Test/Reader/TestDataTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 using Xunit;
 
@@ -7,10 +8,35 @@ namespace Parquet.Test.Reader
 {
    public class TestDataTest : ParquetCsvComparison
    {
+      /// <summary>
+      /// +---+--------+-----------+------------+-------+----------+---------+----------+-------------------------+----------+---------------------+
+      /// |id |bool_col|tinyint_col|smallint_col|int_col|bigint_col|float_col|double_col|date_string_col          |string_col|timestamp_col        |
+      /// +---+--------+-----------+------------+-------+----------+---------+----------+-------------------------+----------+---------------------+
+      /// |4  |true    |0          |0           |0      |0         |0.0      |0.0       |[30 33 2F 30 31 2F 30 39]|[30]      |2009-03-01 00:00:00.0|
+      /// |5  |false   |1          |1           |1      |10        |1.1      |10.1      |[30 33 2F 30 31 2F 30 39]|[31]      |2009-03-01 00:01:00.0|
+      /// |6  |true    |0          |0           |0      |0         |0.0      |0.0       |[30 34 2F 30 31 2F 30 39]|[30]      |2009-04-01 00:00:00.0|
+      /// |7  |false   |1          |1           |1      |10        |1.1      |10.1      |[30 34 2F 30 31 2F 30 39]|[31]      |2009-04-01 00:01:00.0|
+      /// |2  |true    |0          |0           |0      |0         |0.0      |0.0       |[30 32 2F 30 31 2F 30 39]|[30]      |2009-02-01 00:00:00.0|
+      /// |3  |false   |1          |1           |1      |10        |1.1      |10.1      |[30 32 2F 30 31 2F 30 39]|[31]      |2009-02-01 00:01:00.0|
+      /// |0  |true    |0          |0           |0      |0         |0.0      |0.0       |[30 31 2F 30 31 2F 30 39]|[30]      |2009-01-01 00:00:00.0|
+      /// |1  |false   |1          |1           |1      |10        |1.1      |10.1      |[30 31 2F 30 31 2F 30 39]|[31]      |2009-01-01 00:01:00.0|
+      /// +---+--------+-----------+------------+-------+----------+---------+----------+-------------------------+----------+---------------------+
+      /// </summary>
       [Fact]
       public void Alltypes_plain()
       {
-         CompareFiles("alltypes_plain");
+         CompareFiles("alltypes_plain",
+            typeof(int?),
+            typeof(bool?),
+            typeof(int?),
+            typeof(int?),
+            typeof(int?),
+            typeof(long?),
+            typeof(float?),
+            typeof(double?),
+            typeof(string),
+            typeof(string),
+            typeof(DateTime?));
       }
    }
 }
