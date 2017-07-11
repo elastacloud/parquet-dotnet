71b7766bd646ab38107dffd25901d1ad18c6d8a8
Sandy May <sandy@elastacloud.com>
2017-07-11 14:06:37 +0100

    Implement snappy compression

---
+++ src/Parquet/File/Data/SnappyDataReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Snappy.Sharp;
+
+namespace Parquet.File.Data
+{
+    class SnappyDataReader: IDataReader
+    {
+       public byte[] Read(Stream source, int count)
+       {
+          var buffer = new byte[count];
+          source.Read(buffer, 0, count);
+          var snappy = new SnappyDecompressor();
+          var uncompressedBytes = snappy.Decompress(buffer, 0, count);
+          return uncompressedBytes;
+       }
+    }
+}
---
+++ src/Parquet/File/Data/SnappyDataWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Snappy.Sharp;
+
+namespace Parquet.File.Data
+{
+    class SnappyDataWriter : IDataWriter
+    {
+       public void Write(byte[] buffer, Stream destination)
+       {
+          var uncompressedLength = buffer.Length;
+          var snappy = new SnappyCompressor();
+          var compressed = new byte[snappy.MaxCompressedLength(uncompressedLength)];
+          snappy.Compress(buffer, 0, uncompressedLength, compressed);
+          destination.Write(compressed, 0, compressed.Length);
+       }
+    }
+}
