18e2cf39c8a5fc698a479eedf857e8f7aff593dc
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-07-13 12:44:36 +0100

    added docs on dates

--- src/Parquet.Test/EndToEndTypeTest.cs
+++ src/Parquet.Test/EndToEndTypeTest.cs
@@ -42,6 +42,8 @@ namespace Parquet.Test
          var ms = new MemoryStream();
          ParquetWriter.Write(ds, ms);
 
+         new DataSet(new Schema())
+
          ms.Position = 0;
          DataSet ds1 = ParquetReader.Read(ms);
 
