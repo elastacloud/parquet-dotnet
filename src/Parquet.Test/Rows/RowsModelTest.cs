a94a2bd8508f114fee6d132bcf59004461031684
Darren Fuller <darren@elastacloud.com>
2019-01-03 13:01:08 +0000

    refactored test to use public ToString instead of internal methods

--- src/Parquet.Test/Rows/RowsModelTest.cs
+++ src/Parquet.Test/Rows/RowsModelTest.cs
@@ -503,6 +503,31 @@ namespace Parquet.Test.Rows
          jsons.Select(j => JsonConvert.DeserializeObject(j)).ToList();
       }
 
+      [Fact]
+      public void JSON_convert_with_null_arrays()
+      {
+         var t = new Table(new Schema(
+            new DataField<string>("name"),
+            new ListField("observations",
+               new StructField("observation",
+                  new DataField<DateTimeOffset>("date"),
+                  new DataField<IEnumerable<int?>>("values")))));
+
+         t.Add("London", new[]
+         {
+            new Row(new DateTimeOffset(2018, 1, 1, 1, 1, 1, TimeSpan.Zero), new int?[] { 1, 2, 3, 4, 5 })
+         });
+
+         t.Add("Oslo", new[]
+         {
+            new Row(new DateTimeOffset(2018, 1, 1, 1, 1, 1, TimeSpan.Zero), new int?[] { null, null, null, null, null, null, null })
+         });
+
+         string[] jsons = t.ToString("jsq").Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+         Assert.Equal("{'name': 'London', 'observations': [{'date': '01/01/2018 01:01:01 +00:00', 'values': [1, 2, 3, 4, 5]}]}", jsons[0]);
+         Assert.Equal("{'name': 'Oslo', 'observations': [{'date': '01/01/2018 01:01:01 +00:00', 'values': [null, null, null, null, null, null, null]}]}", jsons[1]);
+      }
+
       #endregion
    }
 }
